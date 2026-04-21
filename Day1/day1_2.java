5251e419435a501503a559e7d7f63b89bdae2159
ABHINAV_TIWARI <abhinav.tiwari_cs23@gla.ac.in>
2026-04-21 14:21:14 +0530

    Initial commit

---
+++ Day1/day1_2.java
@@ -0,0 +1,16 @@
+package Day1;
+public class day1_2 {   
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}   
